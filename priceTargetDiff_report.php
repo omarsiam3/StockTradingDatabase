36e2c2aa8c0c0dfae9807daf6e76fead6a684e23
Omar <omar31634@gmail.com>
2021-12-14 00:23:43 -0500

    price target accuracy report

---
+++ priceTargetDiff_report.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$username = 'ofs5049';
+$password = 'omar431';
+$host = 'localhost';
+$dbname = 'ofs5049_431W';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $sql = 'SELECT P.firm, P.price_target, C.price, ((P.price_target-C.price)/C.price)*100) AS percent_ifference FROM price_targets P, companies C ORDER BY dercent_difference ASC;';
+    $q = $pdo->query($sql);
+    $q->setFetchMode(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Could not connect to the database $dbname :" . $e->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accuracy of Price Target</title>
+    </head>
+    <body>
+        <div id="container">
+            <h2>Accuracy of Price Target</h2>
+            <table border=1 cellspacing=5 cellpadding=5>
+                <thead>
+                    <tr>
+                        <th>Firm</th>
+                        <th>Price Target</th>
+                        <th>Current Price</th>
+                        <th>Percent Difference (%)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php while ($row = $q->fetch()): ?>
+                        <tr> 
+                            <td><?php echo htmlspecialchars($row['firm']); ?></td>
+                            <td><?php echo htmlspecialchars($row['price_target']) ?></td>
+                            <td><?php echo htmlspecialchars($row['price']); ?></td>
+                            <td><?php echo htmlspecialchars($row['percent_difference']) ?></td>
+                        </tr>
+                    <?php endwhile; ?>
+                </tbody>
+            </table>
+        </div>
+    </body>
+</html>
